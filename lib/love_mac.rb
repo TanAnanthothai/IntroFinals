6dcc9445375dba4fcc3a887f9c4792f7c157e0fa
Tan Ananthothai <teerakorn.a@gmail.com>
2014-12-02 14:06:04 +0700

    introFinals

--- lib/love_mac.rb
+++ lib/love_mac.rb
@@ -1,37 +1,15 @@
 class Game
 
 	def initialize
-		@rolls = [] # initialize an array to hold the score
+		(1..50).each do |number|
+			loveMac(number)
+		end
 	end
 	
-	def roll pins
-		@rolls.push pins
-	end
-
-	def score
-		result = 0
-		rollIndex = 0
-		10.times do 
-			if @rolls[rollIndex] + @rolls[rollIndex+1] ==10
-				result += 10 + @rolls[rollIndex+2] 
-				rollIndex += 2
-			elsif @rolls[rollIndex]==10
-				result += 10 + @rolls[rollIndex+1] + @rolls[rollIndex+2] 
-				rollIndex += 1
-			else 
-				result += @rolls[rollIndex] + @rolls[rollIndex+1]
-				rollIndex += 2
-			end
-			
-		end
-		puts result
-		result
-	end
-
 	def loveMac(number)
-		puts "lovemac verymuch"
+	 	number
 	end
-
+	
 end
 
 
--- spec/lib/love_mac_spec.rb
+++ spec/lib/love_mac_spec.rb
@@ -4,97 +4,36 @@ require "love_mac"   # name of the class we have just created
 describe 'BowlingGameTest' do
 
 	before do 
-		puts "\nNew Bowling game is created"
-		@game = Game.new
-		(1..5).each do |number|
-			@game.loveMac(1)
-			puts "try"
+		puts "\nThe Love Mac testing is started"
+	end
+	
+	def loveMac(number)
+	 	puts number
+	 	number
+	end
+
+	it('Hello Test') do
+	 	puts "\nPrint the numbers from 1-50"
+	 	game = Game.new
+		(1..50).each do |number|
+
+			expect(game.loveMac(number)).to eq number
+			
+			if number%15==0 then
+		  		puts "HateWindows"
+		   	
+		   	elsif number%5==0 then
+		  		puts "Mac"
+		    	
+		   
+		  	elsif number%3==0 then
+		  		puts "Love"
+
+		  	else 
+		  		puts number
+		   	end
+	
 		end
 	end
 
-	
-
-	it('all 0 games') do
-		puts "\nall 0 games"
-		game = Game.new
-		20.times{game.roll 0}
-		expect(game.score).to eq 0 
-	end
-
-	it('all ones') do
-		puts "\nall ones games"
-		game = Game.new
-		20.times{game.roll 1}
-		expect(game.score).to eq 20
-	end
-
-	it('a spare') do
-		puts "\na spare"
-		@game.roll 5
-		@game.roll 5
-		@game.roll 3
-		17.times{@game.roll 0}
-		expect(@game.score).to eq 16
-	end 
-
-	it('a strike') do
-		puts "\na strike"
-		@game.roll 10
-		@game.roll 4
-		@game.roll 3
-		16.times{@game.roll 0}
-		expect(@game.score).to eq 24
-	end 
-
-	it('all strikes') do
-		puts "\nall strikes"
-		12.times{@game.roll 10}
-		expect(@game.score).to eq 300
-	end
-
-	it('all spares') do
-		puts "\nall spares"
-		21.times{@game.roll 5}
-		expect(@game.score).to eq 150
-	end 
-
-	it ('example score, from excel file') do
-		#frame 1
-		@game.roll 10
-		#frame 2
-		@game.roll 8 
-		@game.roll 1 
-		#28. roll28
-		#frame 3
-		@game.roll 2
-		@game.roll 8
-		#frame 4
-		@game.roll 5 
-		@game.roll 4
-		#52, roll12
-		
-		#frame 5
-		@game.roll 10
-		
-		# #frame 6
-		 @game.roll 10
-		 #9.times{@game.roll 0}
-		#74 roll9
-		# #frame 7
-		  @game.roll 10
-		 
-		# # #frame 8
-		  @game.roll 5
-		 
-		  @game.roll 5
-		#137, 5rolls
-		# # #frame 9
-		  @game.roll 10
-		
-		# # #frame 10
-		 @game.roll 10		
-		2.times{@game.roll 0}
-		expect(@game.score).to eq 177
-
-	end
 end
